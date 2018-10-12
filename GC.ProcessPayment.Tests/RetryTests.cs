086659f2948dfc80a0f0898c4853f6cf1e13dd74
Bogdan Marin <mr.marin.bogdan@gmail.com>
2018-10-12 23:35:49 +0300

    adding more tests

---
+++ GC.ProcessPayment.Tests/RetryTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GC.ProcessPayment.Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GC.ProcessPayment.Tests
+{
+    [TestClass]
+    public class RetryTests
+    {
+        [TestMethod]
+        public void ShouldRetryTwice(){
+            int retry = 0;
+
+            bool result = Retry.Execute(() =>
+            {
+                if (retry < 2)
+                {
+                    retry++;
+                    throw new ArgumentException("Cannot connect to host");
+
+                }
+
+                return true;
+
+            }, new TimeSpan(0, 0, 1), 3, true, true);
+
+            Assert.IsTrue(result, "Should return true after 2 failures");
+            Assert.AreEqual(retry, 2);
+        }
+    }
+}
